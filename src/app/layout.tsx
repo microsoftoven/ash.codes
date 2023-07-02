858bfd849da784856e4a2a0b9a467c2137f30fad
microsoftoven <ashlyn.sandler@gmail.com>
2023-07-02 15:04:29 -0400

    fixing a typo in description

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,17 +8,13 @@ const inter = Inter({ subsets: ["latin"], display: "swap" });
 
 export const metadata = {
   title: "Ash Sandler - SWE",
-  description: "Personal website for Ash Sandler, Senior Software Entineer",
+  description: "Personal website for Ash Sandler, Senior Software Engineer",
   icons: {
     icon: "/favicon.png",
   },
 };
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body className={inter.className}>
